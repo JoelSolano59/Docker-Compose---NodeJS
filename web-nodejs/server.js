72a5ae1db20d06b743c84f7a53a6404d38008a4f
Joel Solano <joel.solano59@gmail.com>
2023-04-21 18:33:56 -0600

    Final Corrections

--- web-nodejs/server.js
+++ web-nodejs/server.js
@@ -28,7 +28,7 @@ app.get('/', (req, res) => {
 	});
 });
 
-app.post('/signup', (req, res) => {
+app.post('/reg', (req, res) => {
 	var EMPLOYEE_ID = 0;
 	var FIRST_NAME = 'Joel';
 	var LAST_NAME = 'Isaias';
@@ -41,48 +41,36 @@ app.post('/signup', (req, res) => {
 	var MANAGER_ID = 100;
 	var DEPARTMENT_ID = 90;
 
-	con.connect(function (err) {
+	// checking user already registered or no
+	connection.query(`SELECT * FROM employees EMAIL = '${EMAIL}' AND PHONE_NUMBER = '${PHONE_NUMBER}'`, function (err, result) {
 		if (err) {
-			console.log(err);
-		}
-		// checking user already registered or no
-		con.query(`SELECT * FROM employees EMAIL = '${EMAIL}' AND PHONE_NUMBER = '${PHONE_NUMBER}'`, function (err, result) {
-			if (err) {
-				console.log(err);
-			};
-			if (Object.keys(result).length > 0) {
-				res.sendFile(__dirname + '/');
-			} else {
-				//creating user page in userPage function
-				function userPage() {
-					res.send(`
-								<!DOCTYPE html>
-								<html lang="en">
-								<head>
-										<title>Docker-nodejs-mysql</title>
-										<meta charset="UTF-8">
-										<meta name="viewport" content="width=device-width, initial-scale=1">
-										<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
-								</head>
-								<body>
-										<div class="container">
-												<h3>Sign up succesfully!</h3>
-										</div>
-								</body>
-								</html>
-								`);
-				}
-			}
-			// inserting new user data
-			var sql = `INSERT INTO employees (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID) VALUES ('${EMPLOYEE_ID}', '${FIRST_NAME}', '${LAST_NAME}', '${EMAIL}', '${PHONE_NUMBER}', '${HIRE_DATE}', '${JOB_ID}', '${SALARY}', '${COMMISSION_PCT}', '${MANAGER_ID}', '${DEPARTMENT_ID}')`;
-			con.query(sql, function (err, result) {
-				if (err) {
-					console.log(err);
-				} else {
-					// using userPage function for creating user page
-					userPage();
-				}
+			res.json({
+				success: false,
+				err
 			});
+		};
+		if (Object.keys(result).length > 0) {
+			rres.json({
+				success: false,
+				err
+			});
+		}
+		// inserting new user data
+		var sql = `INSERT INTO employees (EMPLOYEE_ID, FIRST_NAME, LAST_NAME, EMAIL, PHONE_NUMBER, HIRE_DATE, JOB_ID, SALARY, COMMISSION_PCT, MANAGER_ID, DEPARTMENT_ID) VALUES ('${EMPLOYEE_ID}', '${FIRST_NAME}', '${LAST_NAME}', '${EMAIL}', '${PHONE_NUMBER}', '${HIRE_DATE}', '${JOB_ID}', '${SALARY}', '${COMMISSION_PCT}', '${MANAGER_ID}', '${DEPARTMENT_ID}')`;
+		connection.query(sql, function (err, result) {
+			if (err) {
+				res.json({
+					success: false,
+					err
+				});
+			}
+
+			else {
+				res.json({
+					success: true,
+					rows
+				});
+			}
 		});
 	});
 });
